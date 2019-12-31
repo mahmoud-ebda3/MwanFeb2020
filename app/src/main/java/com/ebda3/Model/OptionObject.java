7a4cd4d0d7112212b0d10fcd9657c4da7eafb98b
Ahmed <ahmed.shaker38@hotmail.com>
2019-12-31 16:44:01 +0200

    init sss

---
+++ app/src/main/java/com/ebda3/Model/OptionObject.java
@@ -0,0 +1,28 @@
+package com.ebda3.Model;
+
+public class OptionObject {
+
+    private int image;
+    private String name;
+
+    public OptionObject(int image, String name) {
+        this.image = image;
+        this.name = name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
